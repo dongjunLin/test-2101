e611ea07fdb899b27964770180d447491e90d842
dongjun <212696646@qq.com>
2021-10-23 20:01:10 +0800

    test first

---
+++ src/main/java/com/zyjt/App.java
@@ -0,0 +1,13 @@
+package com.zyjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
